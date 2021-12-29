cf7a3726b5cd7d992b2e4248d9e887191a34ea31
Sally McGrath <sally@codeyourfuture.io>
2021-12-29 17:45:06 -0500

    add a flexbox to site header
    you are ALLOWED to use flexbox!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="c-site">
       <header className="c-site__header">
         <Logo></Logo>
-        <h1 className='o-type__heading--3'>SASS in React</h1>
+        <h1 className='c-site__name o-type__heading--3'>SASS in React</h1>
       </header>
       <main className='c-site__main'>
         <CardList />
