c1e90b18824bfa9ec6be28fa4efe709a4c9d9a46
Sally McGrath <sally@codeyourfuture.io>
2021-12-30 10:51:41 -0500

    sizing pictures
    Now I've put in the font stuff, update media sizing to rems,  and added a bit more commentary and challenge around the picture sizing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import Logo from './components/Logo';
+import '@csstools/normalize.css';
 import './App.scss';
+import Logo from './components/Logo';
 import CardList from './components/CardList';
 
 function App() {
@@ -7,13 +8,13 @@ function App() {
     <div className="c-site">
       <header className="c-site__header">
         <Logo></Logo>
-        <h1 className='c-site__name o-type__heading--3'>SASS in React</h1>
+        <h1 className='c-site__name'>SASS in React</h1>
       </header>
       <main className='c-site__main'>
-        <h2 class="o-type__invisible">List of cards</h2>
+        <h2 class="o-type__invisible">List of cards showing dummy data</h2>
         <CardList />
       </main>
-      <footer className='c-site__footer'></footer>
+      <footer className='c-site__footer'><h4><a href="https://github.com/CodeYourFuture/CYF-Sass-In-React">← repo</a></h4></footer>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import Picture from './Picture';
 import './Card.scss'
 
 const Card = ({ id, heading, summary, picture, action }) => (
-        <a href={`/manifestos/${id}`} className="c-card">
+        <a href={id} className="c-card">
             <h3 className="c-card__heading">{heading}</h3>
             <p className="c-card__summary">{summary}</p>
             <DivButton className="c-card__action" text={action + ' → '}></DivButton>
